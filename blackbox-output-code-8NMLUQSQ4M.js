5ba32ffb382cfe9d6341b35855cb496523cfb71f
mohdtahir091993 <mohdtahir091993@gmail.com>
2026-01-17 22:05:02 +0530

    Add files via upload

---
+++ blackbox-output-code-8NMLUQSQ4M.js
@@ -0,0 +1,12 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 3000); // Change slide every 3 seconds
